2914c40360583498fc86b4b099bd253ddcbb33db
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-04-02 15:12:06 -0300

    removed some comments

--- src/server.ts
+++ src/server.ts
@@ -27,25 +27,14 @@ const io = socketio(server);
 io.on("connection", (socket) => {
   const { id } = socket.handshake.query;
 
-  // setInterval(() => {
-  //   socket.emit("test", "hello there", () => {
-  //     console.log("mensagem enviada");
-  //   });
-  // }, 1000);
-
   socket.on("disconnect", () => {
     console.log("usuário se desconectou", id);
   });
 });
 
 app.use((request: Request | any, response: Response, next: NextFunction) => {
-  // O io será usado para mandar e enviar mensagens
   request.io = io;
 
-  // Manda para as rotas os usuários conectados
-  // request.connectedUsers = connectedUsers;
-
-  // Para continuar o fluxo da aplicação, se não tiver isso ele para aqui mesmo
   return next();
 });
 
